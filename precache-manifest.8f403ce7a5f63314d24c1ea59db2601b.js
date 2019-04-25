3d950b91ac4f64e7e168a49ef5c023caaedbcdc1
michaelb00m <43150662+michaelb00m@users.noreply.github.com>
2019-04-25 03:04:28 +0200

    Added files

---
+++ precache-manifest.8f403ce7a5f63314d24c1ea59db2601b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bbf769135a55fee75a11d8460261b4",
+    "url": "/hello-world/index.html"
+  },
+  {
+    "revision": "f5470452aec0bd61a447",
+    "url": "/hello-world/static/js/2.4cbfd1a3.chunk.js"
+  },
+  {
+    "revision": "49ea898b7986e66e43e7",
+    "url": "/hello-world/static/js/main.37c6db50.chunk.js"
+  },
+  {
+    "revision": "ffb4ea3617005ab7a82a",
+    "url": "/hello-world/static/js/runtime~main.dc31fdf6.js"
+  }
+]);
